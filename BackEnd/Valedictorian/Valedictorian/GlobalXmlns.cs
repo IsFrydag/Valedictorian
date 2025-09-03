9004e8b22ac3d57eced8074c09395807f1728d05
IsFrydag <freitagheiner3@gmail.com>
2025-09-03 12:22:51 +0200

    Added VS BackEnd Project

---
+++ BackEnd/Valedictorian/Valedictorian/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Valedictorian")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Valedictorian.Pages")]
