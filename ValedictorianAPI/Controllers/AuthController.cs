673d771b9f9b813768a8e08b9f47bde4968cba6b
Jt8021 <jttheun8021@gmail.com>
2025-10-08 13:16:46 +0200

    Submit

---
+++ ValedictorianAPI/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ValedictorianAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("GetLoginName")]
+        public IActionResult GetLoginName()
+        {
+            // Example static data
+            var userData = new
+            {
+                username = "Andy123",
+                age = 29
+            };
+
+            return Ok(userData); // returns JSON { "username": "Andy123", "age": 29 }
+        }
+    }
+}
