a482311c600b2e39456d5081401e60a61d4e8c7d
Jt8021 <jttheun8021@gmail.com>
2025-10-08 16:22:25 +0200

    profile_API / Notification_API
    Added 2 new controllers for profile and Notification.

---
+++ ValedictorianAPI/Controllers/NotivController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ValedictorianAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotivController : ControllerBase
+    {
+            [HttpGet("NotificationRecived")]
+            public IActionResult NotificationRecived()
+            {
+                // Example static data
+                var Message = new
+                {
+                    Message = "No new Notifications"
+                };
+
+                return Ok(Message); // returns JSON { "username": "Andy123", "age": 29 }
+            }
+        
+    }
+}
---
+++ ValedictorianAPI/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ValedictorianAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet("SaveProfileInfo")]
+        public IActionResult SaveProfileInfo()
+        {
+            // Example static data
+            var Message = new
+            {
+                Message = "Piet"
+            };
+
+            return Ok(Message); // returns JSON { "username": "Andy123", "age": 29 }
+        }
+    }
+}
