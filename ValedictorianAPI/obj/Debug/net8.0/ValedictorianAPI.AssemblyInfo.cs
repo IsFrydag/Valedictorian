711d0ddb55879b3371dd50c74ad222a53b5d8d46
mfield-hub <milenkef@gmail.com>
2025-10-12 21:10:33 +0200

    Updated Topics page
    tried to correct the html layout of the columns and work on js to add new topics dynamically

--- ValedictorianAPI/obj/Debug/net8.0/ValedictorianAPI.AssemblyInfo.cs
+++ ValedictorianAPI/obj/Debug/net8.0/ValedictorianAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ValedictorianAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9fb390c0b642d9ba124c4d5dc75640f8e0e53d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59babe0ae64960dff4e519378c0396396816d457")]
 [assembly: System.Reflection.AssemblyProductAttribute("ValedictorianAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ValedictorianAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
